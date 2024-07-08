0edaf73c547ba77c94c884a30227539309b63379
RiansyahTohamba <mriansyah93@gmail.com>
2024-07-09 06:37:24 +0800

    refactor: sharing object with let

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -7,14 +7,15 @@ class Sandwich
 end
 
 RSpec.describe 'An ideal sandwich' do
-    before { @sandwich = Sandwich.new('delicious',[]) }
+    let(:sandwich) { Sandwich.new('delicious',[]) }
+    
     it 'is delicious' do
-        taste = @sandwich.taste
+        taste = sandwich.taste
         expect(taste).to eq('delicious')
     end
     
     it 'lets me add toppings' do
-        toppings = @sandwich.toppings << 'shrimps'
+        toppings = sandwich.toppings << 'shrimps'
         expect(toppings).not_to be_empty
     end
 end
