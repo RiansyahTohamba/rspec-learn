9622d127590b386100575409e3d5abea03ad716e
RiansyahTohamba <mriansyah93@gmail.com>
2024-07-18 17:03:31 +0800

    chore: rename folder

--- expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -1,12 +1,14 @@
 require 'rack/test'
 require 'json'
+require_relative '../../app/api'
 
-def app
-    ExpenseTracker::API.new
-end
 module ExpenseTracker
     RSpec.describe 'Expense Tracker API' do
         include Rack::Test::Methods
+        def app
+            ExpenseTracker::API.new
+        end
+
         it 'records submitted expenses' do
             coffee = {
                 'payee' => 'Starbucks',
