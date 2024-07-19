9701b29d5870dbc33d445e4bd8834346ed1c63a4
RiansyahTohamba <mriansyah93@gmail.com>
2024-07-19 15:04:45 +0800

    refactoring: extract post assertion

--- expense_tracker/app/api.rb
+++ expense_tracker/app/api.rb
@@ -2,5 +2,9 @@ require 'sinatra/base'
 require 'json'
 module ExpenseTracker
     class API < Sinatra::Base
+        post '/expenses' do
+            JSON.generate("expense_id"=>12)
+        end
+
     end
 end
--- expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -8,14 +8,32 @@ module ExpenseTracker
         def app
             ExpenseTracker::API.new
         end
+        
+        def post_expense(expense)
+            post '/expenses', JSON.generate(expense)
+            expect(last_response.status).to eq(200)
+
+            parsed = JSON.parse(last_response.body)
+            expect(parsed).to include('expense_id' => a_kind_of(Integer))
+            expense.merge('id' => parsed['expense_id'])
+        end            
 
         it 'records submitted expenses' do
-            coffee = {
+            expense = {
                 'payee' => 'Starbucks',
                 'amount' => 10,
-                'date'=> '2015-10-02'
+                'date'=> '2014-10-02'
             }
-            post '/expenses', JSON.generate(coffee)
+            post_expense(expense)
+            zoo = {
+                'payee' => 'Zoo',
+                'amount' => 20,
+                'date'=> '2024-07-02'
+            }
+            post_expense(zoo)
+
+            get ''
         end
+
     end
 end
