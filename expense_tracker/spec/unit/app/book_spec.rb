f41e09a63ed8da7b924e059f8ad78e039453a6c9
RiansyahTohamba <mriansyah93@gmail.com>
2024-07-24 11:03:49 +0800

    docs: learn about sharing setup using helper and let

---
+++ expense_tracker/spec/unit/app/book_spec.rb
@@ -0,0 +1,43 @@
+
+class BookShelf
+    attr_reader :books
+    def initialize
+        @books = []
+    end
+    def add(book)
+        @books << book
+    end
+end
+
+RSpec.describe BookShelf do
+    before do
+        @book1 = {author: 'Riansyah', title: 'Filsafat Jerman'}
+        @book2 = {author: 'Bio', title: 'Kemajuan Cina'}
+    end
+    let(:new_shelf) { BookShelf.new}
+
+    def second_shelf 
+        @second_shelf ||= BookShelf.new
+    end
+    
+    it 'should have only adding one book' do
+        new_shelf.add(@book1)
+        expect(new_shelf.books.length).to eq(1)
+    end
+
+    it 'should have only one books again, even we add more books' do
+        new_shelf.add(@book2)
+        expect(new_shelf.books.length).not_to eq(2)
+    end
+
+    it 'should have only adding one book by using helper' do
+        second_shelf.add(@book1)
+        expect(second_shelf.books.length).to eq(1)
+    end
+
+    it 'should become only one books in the shelf by using helper' do
+        second_shelf.add(@book2)
+        expect(second_shelf.books[0][:title]).to eq('Kemajuan Cina')
+        expect(second_shelf.books.length).to eq(1)
+    end
+end
