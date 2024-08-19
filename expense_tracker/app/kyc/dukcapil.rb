239230f91fe08a74bf8c276776fd6add37b529f2
RiansyahTohamba <mriansyah93@gmail.com>
2024-08-19 16:03:26 +0800

    mocking dukcapil third data service

--- expense_tracker/app/kyc/dukcapil.rb
+++ expense_tracker/app/kyc/dukcapil.rb
@@ -1,17 +1,37 @@
 require 'json'
+require 'ostruct'
 
 module ExpenseTracker
     DukcapilResult = Struct.new(:status_code, :content, :error_message)
+
     class Dukcapil
+        FR_URL = 'https://api.dukcapil.go.id/fr_check'
         def initialize
         end
+
+        def fr_check(debitur)
+            uri = URI(FR_URL)
+            request = Net::HTTP::Post.new(uri)
+            request['Content-Type'] = 'application/json'
+            request.body = { nik: debitur[:nik] }.to_json
+        
+            response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
+              http.request(request)
+            end
+            
+            parsed_response = JSON.parse(response.body)
+
+            if parsed_response['content'] && parsed_response['content']['score']
+                OpenStruct.new(success?: true, data: parsed_response['content'])
+            else
+                OpenStruct.new(success?: false, error_message: 'verification failed')
+            end
+        end
+
         def text_check  
             result = {NAME: true}
             DukcapilResult.new(200,result,nil)
         end
-        def fr_check(debitur)
-            result = {score: 0.8}
-            DukcapilResult.new(200,result,nil)
-        end
+
     end
 end
