44fd2abd8686595116655a3eb1bb44e7e46d3d85
RiansyahTohamba <mriansyah93@gmail.com>
2024-08-19 10:07:32 +0800

    feat: find by date - solve expenses_on - content-type JSON must explicit

--- expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
+++ expense_tracker/spec/acceptance/expense_tracker_api_spec.rb
@@ -3,41 +3,46 @@ require 'json'
 require_relative '../../app/api'
 
 module ExpenseTracker
-    RSpec.describe 'Expense Tracker API', :db do
-        include Rack::Test::Methods
-        def app
-            ExpenseTracker::API.new(ledger: Ledger.new)
-        end
-        
-        def post_expense(expense)
-            post '/expenses', JSON.generate(expense)
-            expect(last_response.status).to eq(200)
+  RSpec.describe 'Expense Tracker API', :db do
+    include Rack::Test::Methods
 
-            parsed = JSON.parse(last_response.body)
-            expect(parsed).to include('expense_id' => a_kind_of(Integer))
-            expense.merge('id' => parsed['expense_id'])
-        end            
-
-        it 'records submitted expenses' do
-            pending 'require to persist the expenses'
-            coffee = {
-                'payee' => 'Starbucks',
-                'amount' => 10,
-                'date'=> '2014-10-02'
-            }
-            post_expense(coffee)
-            zoo = {
-                'payee' => 'Zoo',
-                'amount' => 20,
-                'date'=> '2024-07-02'
-            }
-            post_expense(zoo)
-
-            get '/expenses/2024-10-02'
-            expect(last_response.status).to eq(200)
-            expenses = JSON.parse(last_response.body)
-            expect(expenses).to contain_exactly(coffee,zoo)
-        end
-
+    def app
+      ExpenseTracker::API.new
     end
+
+    def post_expense(expense)
+      post '/expenses',JSON.generate(expense), { 'CONTENT_TYPE' => 'application/json' }
+      expect(last_response.status).to eq(200)
+
+      parsed = JSON.parse(last_response.body)
+      expect(parsed).to include('expense_id' => a_kind_of(Integer))
+      expense.merge('id' => parsed['expense_id'])
+    end
+
+    it 'records submitted expenses' do
+      coffee = post_expense(
+        'payee'  => 'Starbucks',
+        'amount' => 5.75,
+        'date'   => '2017-06-10'
+      )
+
+      zoo = post_expense(
+        'payee'  => 'Zoo',
+        'amount' => 15.25,
+        'date'   => '2017-06-10'
+      )
+
+      groceries = post_expense(
+        'payee'  => 'Whole Foods',
+        'amount' => 95.20,
+        'date'   => '2017-06-11'
+      )
+
+      get '/expenses/2017-06-10'
+      expect(last_response.status).to eq(200)
+
+      expenses = JSON.parse(last_response.body)
+      expect(expenses).to contain_exactly(coffee, zoo)
+    end
+  end
 end
--- expense_tracker/spec/unit/app/dukcapil_kyc_spec.rb
+++ expense_tracker/spec/unit/app/dukcapil_kyc_spec.rb
@@ -4,13 +4,15 @@ end
 RSpec.describe DukcapilKYC do
     describe 'POST /text_check' do
         context 'when dukcapil successfully sent respond' do
-            it 'returns the search_score with the value greater than 80' 
-            it 'respond with a 200 status'
+            it 'returns the search_score with the value greater than 80' do
+
+            end
+            # it 'respond with a 200 status'
         end
-        context 'when dukcapil cannot identify debitur' do
-            it 'respond with a 400 status (wrong debts)'
+        # context 'when dukcapil cannot identify debitur' do
+        #     it 'respond with a 400 status (wrong debts)'
             
-        end
+        # end
     end
     describe 'POST /fr_check'
 end
